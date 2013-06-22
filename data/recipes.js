cd38741771a9b1a1bde85d476b67b30625e40881
urobo <rico.sleeps@gmail.com>
2013-06-22 16:40:53 +0200

    recipe.ejs single recipe view

--- data/recipes.js
+++ data/recipes.js
@@ -7,7 +7,7 @@ exports.data = [
 			'8 oz marinara sauce',
 			'Parmasean cheese'
 			],
-		'directions': 'Boil the spaghetti in rapidly boiling, salted water. Cook until al dente, '
+		'directions': 'Boil the spaghetti in rapidly boiling, salted water. Cook until al dente, then drain and rinse in cool water. Warm marinara sauce in a separate saucepan. Serve marinara sauce on top of spaghetti and grate parmasean cheese on top. Serves 2-3.'
 	},
 	{
 		'url' : 'turkey-sandwich',
--- script.js
+++ script.js
@@ -16,7 +16,17 @@ app.get('/recipes', function(req,res){
 });
 
 app.get('/recipes/:title', function(req,res){
-	res.send('<h1>' + req.params.title + '</h1>');
+	var recipe = recipes.filter(function (recipe){
+		return (recipe.url === req.params.title);
+	});
+	if (recipe.length > 0){
+		recipe = recipe[0];
+		recipe.title = 'Clever Kitchens - Recipe';
+		res.render('recipe.ejs',recipe);
+	} else {
+		res.status(404).render('error.ejs', {title: 'Recipe not found!'});
+	}
+	
 });
 
 app.get('/*', function(req,res){
